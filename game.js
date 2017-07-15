c666f5bc2ab9e74b9c05e215ab50b7336228f0fd
Jackson Lango <jacksonlango@gmail.com>
2017-07-15 14:15:24 -0700

    hazards chase and combine

--- game.js
+++ game.js
@@ -3,8 +3,6 @@ if(!PIXI.utils.isWebGLSupported()){
   type = "canvas"
 }
 
-PIXI.utils.sayHello(type)
-
 //Create the renderer
 var renderer = PIXI.autoDetectRenderer(
   256, 256,
@@ -30,26 +28,45 @@ const RIGHT = 39;
 
 const HERO_SPEED = 1;
 const HERO_MAX_SPEED = 5;
+const HERO_FRICTION = .95;
 const GRAVITY = .2;
 const JUMP = -5;
-const FRICTION = .95;
+const HAZARD_FRICTION = .98;
+
 
 var hero;
 var heroVelocityX = 0;
 var heroVelocityY = 0;
 
-
+var hazards = [];
+var hazardsVelocityX = [];
+var hazardsVelocityY = [];
+var hazardsSpeed = [];
 
 
 
 PIXI.loader
-  .add("img/hero.png")
+  .add(["img/hero.png", "img/hazard.png"])
   .load(setup);
 
 function setup() {
   hero = new PIXI.Sprite(
     PIXI.loader.resources["img/hero.png"].texture
   );
+  hero.x = 200;
+
+  for (var i = 0; i < 5; i++) {
+    var hazard = new PIXI.Sprite(
+      PIXI.loader.resources["img/hazard.png"].texture
+    )
+    stage.addChild(hazard);
+    hazards.push(hazard);
+    hazardsVelocityX.push(0);
+    hazardsVelocityY.push(0);
+    hazardsSpeed.push(2);
+    hazard.x = Math.random() * 500;
+    hazard.y = Math.random() * 500;
+  }
 
   stage.addChild(hero);
 
@@ -57,16 +74,16 @@ function setup() {
   gameLoop();
 }
 
-var keyLeft = keyboard(LEFT);
-var keyRight = keyboard(RIGHT);
-var keyUp = keyboard(UP);
+const keyLeft = keyboard(LEFT);
+const keyRight = keyboard(RIGHT);
+const keyUp = keyboard(UP);
 
 
 
 function gameLoop() {
   requestAnimationFrame(gameLoop);
 
-  heroVelocityX *= FRICTION;
+  heroVelocityX *= HERO_FRICTION;
   heroVelocityY += GRAVITY;
 
   if (keyRight.isDown) {
@@ -79,25 +96,68 @@ function gameLoop() {
     heroVelocityY = JUMP;
   }
   
-  // limit speed
-  if (heroVelocityX > HERO_MAX_SPEED) {
-    heroVelocityX = HERO_MAX_SPEED;
-  } else if (heroVelocityX < -HERO_MAX_SPEED) {
-    heroVelocityX = -HERO_MAX_SPEED;
-  }
-
+  heroVelocityX = limit(heroVelocityX, -HERO_MAX_SPEED, HERO_MAX_SPEED);
 
   hero.x += heroVelocityX;
   hero.y += heroVelocityY;
 
+  for (var i = 0; i < hazards.length; i++) {
+    if (!hazards[i].visible) {
+      setupNewHazard(hazards[i]);
+    }
+    hazardsVelocityX[i] *= HAZARD_FRICTION;
+    hazardsVelocityY[i] *= HAZARD_FRICTION;
 
+    const lengthToHero = vectorLength(
+      hero.x - hazards[i].x, 
+      hero.y - hazards[i].y,
+    );
+    hazardsVelocityX[i] += (hero.x - hazards[i].x) / lengthToHero * .5;
+    hazardsVelocityY[i] += (hero.y - hazards[i].y) / lengthToHero * .5;
+
+    hazards[i].x += hazardsVelocityX[i];
+    hazards[i].y += hazardsVelocityY[i];
+
+    // check hazard collision
+    for (var j = 0; j < hazards.length; j++) {
+      if (i == j || !hazards[j].visible) {
+        continue;
+      }
+      if (vectorLength(hazards[j].x - hazards[i].x, hazards[j].y - hazards[i].y) < 10) {
+        hazards[j].visible = false;
+        hazards[i].scale.x += hazards[j].scale.x;
+        hazards[i].scale.y += hazards[j].scale.y;
+      }
+    }
+  }
   renderer.render(stage);
 }
 
 
+function setupNewHazard(hazard) {
+  hazard.visible = true;
+  hazard.y = Math.random() * 500;
+  if (Math.random() < .5) {
+    hazard.x = -50;
+  } else {
+    hazard.x = 1000;
+  }
+}
 
 
+// utils
+function limit(val, min, max) {
+  if (val > max) {
+    return max;
+  } else if (val < min) {
+    return min;
+  }
+  return val;
+}
 
+function vectorLength(x, y) {
+  return Math.sqrt(x * x + y * y);
+}
 
 
 // keyboard class
